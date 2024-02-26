5ace17d123bb831163870649c5aee3e5ccf94c54
TomislavPilip <tpilipac@gmail.com>
2024-02-27 00:02:47 +0100

    update

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,9 @@
-///////////////////////////ABOUT ME TABS/////////////////////////
+///////////////////////////HAMBURGER JQuery/////////////////////////
+const hamburgerButton = document.querySelector(".hamburger-icon");
+const hamburgerMenu = document.querySelector(".side-bar-menu");
+
+$(document).ready(function () {
+  $(".hamburger-icon").click(function () {
+    $(".side-bar-menu").toggle(1000);
+  });
+});
